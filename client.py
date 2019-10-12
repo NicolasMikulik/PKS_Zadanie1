a021076e57ebf6f3c4496a311dfc885be17dd43e
Nicolas Mikulik <nicolas.mikulik01@gmail.com>
2019-10-12 14:04:09 +0200

    First successful reception of sent file

--- client.py
+++ client.py
@@ -1,30 +1,37 @@
 import math
 import socket
 import struct
-from binascii import crc32
+import sys
+
 # Create a UDP socket
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-
-server_address = ('localhost', 10000)
-print('starting up on {} port {}'.format(*server_address))
-message = b'This is our message. It will be sent all at once'
-sock.sendto(message,server_address)
-fragIndex=0 #reset fragment indexing
-fragSize = int(input('Fragment size: '))
-
-file_name = "/home/nicolas/PycharmProjects/Coursera Assignments/romeo.txt"
-f=open(file_name,"rb")
-contents = f.read()
-fragCount = math.ceil(len(contents) / fragSize)
-
+try:
+    mysocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    print("Client socket created")
+except socket.error:
+    print("Failed to create socket")
+    exit()
+read_file = "/home/nicolas/PycharmProjects/Coursera Assignments/romeo.txt"
+server_address = ('localhost', 8484)
+print(str(sys.getsizeof(server_address)))
+msg_type = 1
+data_length = 0
+frag_index = 0
+frag_size = 50  # input("Enter fragment size: ")
+mysocket.sendto(str(frag_size).encode(), server_address)
+print(mysocket.recvfrom(512)[0].decode())
+file = open(read_file, "rb")
+contents = file.read()
+file.close()
+file_size = len(contents)
+frag_count = math.ceil(file_size / int(frag_size))
+print("File of size " + str(file_size) + " is being sent in "+str(frag_count)+" datagrams")
 while contents:
     data = bytearray()
-    data.extend(contents[:fragSize])
-    fragIndex += 1
-    crc = crc32(data)
-    header = struct.pack('!hlll', 3, fragSize, fragIndex, fragCount, crc)
-    sock.sendto(header + bytearray(data), server_address)
-    contents = contents[fragSize:]
-
-print('closing socket')
-sock.close()
+    data.extend(contents[:frag_size])
+    data_length = len(data)
+    frag_index += 1
+    header = struct.pack('BHHH', msg_type, data_length, frag_index, frag_count)
+    mysocket.sendto(header + bytearray(data), server_address)
+    print(mysocket.recvfrom(512)[0].decode())
+    contents = contents[frag_size:]
+mysocket.close()
--- server.py
+++ server.py
@@ -1,42 +1,48 @@
 import socket
+import sys
+from struct import *
 
 # Create a UDP socket
 import struct
-
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-
-# Bind the socket to the port
-server_address = ('localhost', 10000)
-print('starting up on {} port {}'.format(*server_address))
-sock.bind(server_address)
-data, server = sock.recvfrom(4096)
-print('received {!r}'.format(data))
-received_chunks = 0
-rec_list = []
+try:
+    mysocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    print("Socket created")
+except socket.error:
+    print("Failed to create socket")
+    exit()
+server_address = ('localhost', 8484)
+try:  # Bind the socket to the port
+    mysocket.bind(server_address)
+    print('Starting up on {} port {}'.format(*server_address)+ ". Waiting for fragment size.")
+except socket.error:
+    print("Failed to bind socket")
+struct_header_size = calcsize('BHHH')
+address_size = sys.getsizeof(server_address)
+save_path = "/home/nicolas/PycharmProjects/pks_zadanie1"
+fragSize_msg = mysocket.recvfrom(512)
+mysocket.sendto("Roger".encode(), fragSize_msg[1])
+fragSize = int(fragSize_msg[0].decode())
+received_file = bytearray()
+print("Client set fragment size to: "+str(fragSize))
+received_frag = 0
 while True:
-    data, addr = sock.recvfrom(65535)
-    header = data[:18]
-    data = data[18:]
-    (mType, fragSize, fragIndex, fragCount, crc) = struct.unpack('!hIII', header)
-
-    print(
-        '\nTyp: ' + str(mType) +
-        '\nFragSize: ' + str(fragSize) +
-        '\nFragIndex: ' + str(fragIndex) +
-        '\nFragCount: ' + str(fragCount) +
-        '\nCRC: ' + str(crc)
-    )
-
-    if len(rec_list) < fragCount:
-        need_to_add = fragCount - len(rec_list)
-        rec_list.extend([''] * need_to_add)  # empty list for messages of size fragCount
-    rec_list[fragIndex - 1] = data
-
-    received_chunks += 1
-    if received_chunks == fragCount:
-        break  # This is where the second while loop ends
-
-if mType == 3:
-    content = b''.join(rec_list)
-    f = open('output.txt', 'wb')
-    f.write(content)
+    dataStream = mysocket.recvfrom(fragSize+struct_header_size+address_size)
+    data = dataStream[0]
+    addr = dataStream[1]
+    header = data[:8]
+    reply = "Server_Reply: " + str(len(data)) + " characters received"
+    mysocket.sendto(reply.encode(), addr)
+    print("Message["+addr[0]+"] - "+data[8:].decode().strip())
+    received_file += data[8:]
+    print(received_file.decode())
+    (msg_type, data_length, frag_index, frag_count) = struct.unpack('BHHH', header)
+    print(msg_type, data_length, frag_index, frag_count)
+    received_frag += 1
+    if received_frag == frag_count:
+        print("All data received")
+        break
+if msg_type == 1:
+    write_file = open('/home/nicolas/PycharmProjects/pks_zadanie1/received.txt', 'wb')
+    write_file.write(received_file)
+    write_file.close()
+mysocket.close()
